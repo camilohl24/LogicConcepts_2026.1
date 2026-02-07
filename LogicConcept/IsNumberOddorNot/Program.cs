e0d0e9a59f72e06c263b70e5b6124e043ee18fc5
camilohl24 <juancamilohernandezhl@gmail.com>
2026-02-07 16:27:58 -0500

    ejercicio numero 1

---
+++ LogicConcept/IsNumberOddorNot/Program.cs
@@ -0,0 +1,32 @@
+﻿var numberString = string.Empty;
+do
+{
+    Console.WriteLine("Ingrese un numero o presiona la palabra salir");
+    numberString = Console.ReadLine();
+    var numberInt = 0;
+    if (numberString.ToLower() == "salir")
+    {
+        continue;
+    }
+    if( int.TryParse(numberString,  out numberInt))
+    {
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"El numero {numberInt} es par");
+        }
+        else
+        {
+            Console.WriteLine($"El numero {numberInt} es impar");
+        }
+
+    }
+else
+    {
+        Console.WriteLine($"El numero {numberString} no es un numero entero");
+    }
+
+
+} while(numberString.ToLower() != "salir");
+Console.WriteLine("game over ");
+
+
